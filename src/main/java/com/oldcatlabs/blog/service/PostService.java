20e543d4a58e105ec909c4bfdfc0c792ca6ffa1f
mfauzirh <mfauzirizkih@gmail.com>
2026-01-16 11:18:17 +0700

    refactor to use repository

--- src/main/java/com/oldcatlabs/blog/service/PostService.java
+++ src/main/java/com/oldcatlabs/blog/service/PostService.java
@@ -1,79 +1,58 @@
 package com.oldcatlabs.blog.service;
 
 import com.oldcatlabs.blog.entity.Post;
+import com.oldcatlabs.blog.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.time.Instant;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
 
-    Post post1 = new Post(1, "title1", "slug1");
-    Post post2 = new Post(2, "title2", "slug2");
-    List<Post> posts = new ArrayList<>(Arrays.asList(post1, post2));
+    private final PostRepository postRepository;
 
     public List<Post> getPosts() {
-        return posts;
+        return postRepository.findAll();
     }
 
     public Post getPostBySlug(String slug) {
-        return posts.stream()
-                .filter(p -> p.getSlug().equals(slug))
-                .findFirst()
+        return postRepository
+                .findFirstBySlug(slug)
                 .orElse(null);
     }
 
     public Post createPost(Post post) {
-        posts.add(post);
-        return post;
+        post.setCreatedAt(Instant.now().getEpochSecond());
+        return postRepository.save(post);
     }
 
     public Post updatePostBySlug(String slug, Post post) {
-        Post savedPost = posts.stream()
-                .filter(p -> p.getSlug().equals(slug))
-                .findFirst()
-                .orElse(null);
+        Post savedPost = postRepository.findFirstBySlug(slug).orElse(null);
 
-        if (savedPost == null) {
-            return null;
-        }
+        if (savedPost == null) return null;
 
-        posts.remove(savedPost);
-
-        savedPost.setTitle(post.getTitle());
-        savedPost.setSlug(post.getSlug());
-        posts.add(savedPost);
-
-        return savedPost;
+        return postRepository.save(post);
     }
 
     public Boolean deletePostBySlug(Integer id) {
-        Post savedPost = posts.stream()
-                .filter(p -> p.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+        Post savedPost = postRepository.findById(id).orElse(null);
 
-        if (savedPost == null) {
-            return false;
-        }
+        if (savedPost == null) return false;
 
-        posts.remove(savedPost);
+        postRepository.deleteById(id);
         return true;
     }
 
     public Post publishPostById(Integer id) {
-        Post savedPost = posts.stream()
-                .filter(p -> p.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+        Post savedPost = postRepository.findById(id).orElse(null);
 
-        if (savedPost == null) {
-            return null;
-        }
+        if (savedPost == null) return null;
 
         savedPost.setPublished(true);
-        return savedPost;
+        savedPost.setPublishedAt(Instant.now().getEpochSecond());
+        return postRepository.save(savedPost);
     }
 }
